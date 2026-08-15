53d414513efb951933a73b07f1f75a1707664b68
Robit-space <207705032+Robit-space@users.noreply.github.com>
2026-08-15 20:20:49 +0800

    Refine PCL-style top bar UI

--- Sources/App/PCLTopBarView.h
+++ Sources/App/PCLTopBarView.h
@@ -1,7 +1,23 @@
 #import <UIKit/UIKit.h>
-typedef NS_ENUM(NSInteger, PCLPageType) { PCLPageTypeLaunch = 0, PCLPageTypeDownload, PCLPageTypeMultiplayer, PCLPageTypeSettings
+
+typedef NS_ENUM(NSInteger, PCLPageType) {
+    PCLPageTypeLaunch = 0,
+    PCLPageTypeDownload,
+    PCLPageTypeMultiplayer,
+    PCLPageTypeSettings
 };
-@class PCLTopBarView; @protocol PCLTopBarViewDelegate <NSObject> - (void)topBarView:(PCLTopBarView *)topBar didSelectPage:(PCLPageType)page; @end @interface 
-PCLTopBarView : UIView @property (nonatomic, weak) id<PCLTopBarViewDelegate> delegate; @property (nonatomic, assign) PCLPageType selectedPage; - 
-(void)selectPage:(PCLPageType)page animated:(BOOL)animated;
+
+@class PCLTopBarView;
+
+@protocol PCLTopBarViewDelegate <NSObject>
+- (void)topBarView:(PCLTopBarView *)topBar didSelectPage:(PCLPageType)page;
+@end
+
+@interface PCLTopBarView : UIView
+
+@property (nonatomic, weak) id<PCLTopBarViewDelegate> delegate;
+@property (nonatomic, assign) PCLPageType selectedPage;
+
+- (void)selectPage:(PCLPageType)page animated:(BOOL)animated;
+
 @end
--- Sources/App/PCLTopBarView.m
+++ Sources/App/PCLTopBarView.m
@@ -1,57 +1,53 @@
 #import "PCLTopBarView.h"
-@interface PCLTopBarView () @property (nonatomic, strong) UIStackView *buttonStack; @property (nonatomic, strong) UIView *selectionIndicator; @property 
-(nonatomic, strong) NSArray<UIButton *> *buttons; @end @implementation PCLTopBarView - (instancetype)initWithFrame:(CGRect)frame {
+@interface PCLTopBarView () @property (nonatomic, strong) UIStackView *buttonStack; @property (nonatomic, strong) NSArray<UIButton *> *buttons; @end
+@implementation PCLTopBarView
+- (UIColor *)pclThemeColor {
+    return [UIColor colorWithRed:19.0/255.0 green:112.0/255.0 blue:243.0/255.0 alpha:1.0];
+}
+- (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame]; if (self) { [self setupView];
     }
     return self;
 }
-- (void)setupView { self.backgroundColor = [UIColor systemBackgroundColor]; NSArray<NSString *> *titles = @[ @"启动", @"下载", @"联机", @"设置" ]; 
-    NSMutableArray<UIButton *> *buttons = [NSMutableArray array]; self.buttonStack = [[UIStackView alloc] init]; self.buttonStack.axis = 
-    UILayoutConstraintAxisHorizontal; self.buttonStack.alignment = UIStackViewAlignmentCenter; self.buttonStack.spacing = 28.0; 
+- (void)setupView { self.backgroundColor = [self pclThemeColor];
+    NSArray<NSString *> *titles = @[ @"启动", @"下载", @"联机", @"设置" ];
+    NSMutableArray<UIButton *> *buttons = [NSMutableArray array];
+    self.buttonStack = [[UIStackView alloc] init]; self.buttonStack.axis =
+    UILayoutConstraintAxisHorizontal; self.buttonStack.alignment = UIStackViewAlignmentCenter; self.buttonStack.spacing = 8.0;
     self.buttonStack.translatesAutoresizingMaskIntoConstraints = NO; [self addSubview:self.buttonStack]; for (NSInteger i = 0; i < titles.count; i++) {
-        UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem]; [button setTitle:titles[i] forState:UIControlStateNormal]; button.tag = i; 
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem]; [button setTitle:titles[i] forState:UIControlStateNormal]; button.tag = i;
         button.titleLabel.font =
-            [UIFont systemFontOfSize:17 weight:UIFontWeightMedium]; [button addTarget:self action:@selector(pageButtonPressed:) 
+        button.layer.cornerRadius = 13.5;
+        button.clipsToBounds = YES;
+        button.contentEdgeInsets = UIEdgeInsetsMake(5.0, 14.0, 5.0, 14.0);
+        [UIFont systemFontOfSize:17 weight:UIFontWeightMedium]; [button addTarget:self action:@selector(pageButtonPressed:)
          forControlEvents:UIControlEventTouchUpInside];
         [self.buttonStack addArrangedSubview:button]; [buttons addObject:button];
     }
-    self.buttons = buttons; self.selectionIndicator = [[UIView alloc] init]; self.selectionIndicator.backgroundColor = [UIColor systemBlueColor]; 
-    self.selectionIndicator.layer.cornerRadius = 1.5; self.selectionIndicator.translatesAutoresizingMaskIntoConstraints = NO; [self 
-    addSubview:self.selectionIndicator]; [NSLayoutConstraint activateConstraints:@[
-        [self.buttonStack.centerXAnchor constraintEqualToAnchor:self.centerXAnchor], [self.buttonStack.centerYAnchor constraintEqualToAnchor:self.centerYAnchor], 
-        [self.selectionIndicator.bottomAnchor
-            constraintEqualToAnchor:self.bottomAnchor], [self.selectionIndicator.heightAnchor constraintEqualToConstant:3.0], 
-        [self.selectionIndicator.widthAnchor
-            constraintEqualToConstant:38.0] ]]; self.selectedPage = PCLPageTypeLaunch; [self updateButtonAppearance];
+    self.buttons = buttons;  [NSLayoutConstraint activateConstraints:@[
+        [self.buttonStack.centerXAnchor constraintEqualToAnchor:self.centerXAnchor], [self.buttonStack.centerYAnchor constraintEqualToAnchor:self.centerXAnchor],
+            constraintEqualToAnchor:self.bottomAnchor],
+        self.selectedPage = PCLPageTypeLaunch; [self updateButtonAppearance];
 }
-- (void)pageButtonPressed:(UIButton *)sender { PCLPageType page = (PCLPageType)sender.tag; [self selectPage:page animated:YES]; if ([self.delegate 
+- (void)pageButtonPressed:(UIButton *)sender { PCLPageType page = (PCLPageType)sender.tag; [self selectPage:page animated:YES]; if ([self.delegate
     respondsToSelector:
          @selector(topBarView:didSelectPage:)]) { [self.delegate topBarView:self didSelectPage:page];
     }
 }
 - (void)selectPage:(PCLPageType)page animated:(BOOL)animated { if (page < 0 || page >= self.buttons.count) { return;
     }
-    _selectedPage = page; [self updateButtonAppearance]; UIButton *button = self.buttons[page]; void (^changes)(void) = ^{ self.selectionIndicator.center = 
-            CGPointMake(button.center.x,
-                        self.selectionIndicator.center.y);
-    };
-    if (animated) { [UIView animateWithDuration:0.20 animations:changes];
-    } else {
-        changes();
-    }
+    _selectedPage = page; [self updateButtonAppearance]; UIButton *button = self.buttons[page];
 }
-- (void)updateButtonAppearance { for (NSInteger i = 0; i < self.buttons.count; i++) { UIButton *button = self.buttons[i]; if (i == self.selectedPage) { [button 
-            setTitleColor:[UIColor labelColor]
-                         forState:UIControlStateNormal]; button.titleLabel.font = [UIFont systemFontOfSize:17 weight:UIFontWeightSemibold];
+- (void)updateButtonAppearance { for (NSInteger i = 0; i < self.buttons.count; i++) { UIButton *button = self.buttons[i]; if (i == self.selectedPage) { [button
+                       setTitleColor:[self pclThemeColor]
+                           forState:UIControlStateNormal];
+                          button.titleLabel.font = [UIFont systemFontOfSize:15 weight:UIFontWeightSemibold];
+                          button.backgroundColor = UIColor.whiteColor;
         } else {
-            [button setTitleColor:[UIColor secondaryLabelColor] forState:UIControlStateNormal]; button.titleLabel.font = [UIFont systemFontOfSize:17 
+            [button setTitleColor:[self pclThemeColor] forState:UIControlStateNormal]; button.titleLabel.font = [UIFont systemFontOfSize:15
                                   weight:UIFontWeightRegular];
+            button.backgroundColor = UIColor.clearColor;
         }
-    }
-}
-- (void)layoutSubviews { [super layoutSubviews]; if (self.selectedPage < self.buttons.count) { UIButton *button = self.buttons[self.selectedPage]; 
-        self.selectionIndicator.center =
-            CGPointMake(button.center.x, CGRectGetHeight(self.bounds) - 1.5);
     }
 }
 @end
