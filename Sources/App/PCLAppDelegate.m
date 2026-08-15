56aec711e67da3a2c11567f1220f4540060da1b9
Robit-space <207705032+Robit-space@users.noreply.github.com>
2026-08-15 23:30:42 +0800

    Fix fullscreen top bar and whitespace

--- Sources/App/PCLAppDelegate.m
+++ Sources/App/PCLAppDelegate.m
@@ -1,22 +1,22 @@
 #import "PCLAppDelegate.h"
 #import "PCLRootViewController.h"
-@implementation PCLAppDelegate - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions { self.window = 
+@implementation PCLAppDelegate - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions { self.window =
     [[UIWindow alloc]
-        initWithFrame:[UIScreen mainScreen].bounds]; PCLRootViewController *root = [[PCLRootViewController alloc] init]; self.window.rootViewController = root; 
+        initWithFrame:[UIScreen mainScreen].bounds]; PCLRootViewController *root = [[PCLRootViewController alloc] init]; self.window.rootViewController = root;
     [self.window makeKeyAndVisible]; UIView *splash =
-        [[UIView alloc] initWithFrame:self.window.bounds]; splash.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight; if 
+        [[UIView alloc] initWithFrame:self.window.bounds]; splash.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight; if
     (@available(iOS 13.0, *)) {
         splash.backgroundColor = self.window.traitCollection.userInterfaceStyle == UIUserInterfaceStyleDark ? UIColor.blackColor
             : UIColor.whiteColor;
     } else {
         splash.backgroundColor = UIColor.whiteColor;
     }
-    UIImage *icon = [UIImage imageNamed:@"AppIcon"]; UIImageView *logo = [[UIImageView alloc] initWithImage:icon]; logo.contentMode = 
-    UIViewContentModeScaleAspectFit; logo.translatesAutoresizingMaskIntoConstraints = NO; [splash addSubview:logo]; [self.window addSubview:splash]; 
+    UIImage *icon = [UIImage imageNamed:@"SplashLogo"]; UIImageView *logo = [[UIImageView alloc] initWithImage:icon]; logo.contentMode =
+    UIViewContentModeScaleAspectFit; logo.translatesAutoresizingMaskIntoConstraints = NO; [splash addSubview:logo]; [self.window addSubview:splash];
     [NSLayoutConstraint activateConstraints:@[
-        [logo.centerXAnchor constraintEqualToAnchor:splash.centerXAnchor], [logo.centerYAnchor constraintEqualToAnchor:splash.centerYAnchor], [logo.widthAnchor 
+        [logo.centerXAnchor constraintEqualToAnchor:splash.centerXAnchor], [logo.centerYAnchor constraintEqualToAnchor:splash.centerYAnchor], [logo.widthAnchor
         constraintEqualToConstant:180.0], [logo.heightAnchor constraintEqualToConstant:180.0]
-    ]]; dispatch_after( dispatch_time( DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC) ), dispatch_get_main_queue(), ^{ [UIView animateWithDuration:0.45 
+    ]]; dispatch_after( dispatch_time( DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC) ), dispatch_get_main_queue(), ^{ [UIView animateWithDuration:0.45
                          animations:^{
             splash.alpha = 0.0;
         } completion:^(BOOL finished) {
--- Sources/App/PCLRootViewController.m
+++ Sources/App/PCLRootViewController.m
@@ -1,33 +1,50 @@
 #import "PCLRootViewController.h"
 #import "PCLTopBarView.h"
-@interface PCLRootViewController () <PCLTopBarViewDelegate> @property (nonatomic, strong) PCLTopBarView *topBar; @property (nonatomic, strong) UIView 
+@interface PCLRootViewController () <PCLTopBarViewDelegate> @property (nonatomic, strong) PCLTopBarView *topBar; @property (nonatomic, strong) UIView
 *contentView; @property (nonatomic, strong) UILabel *pageLabel; @end @implementation PCLRootViewController - (void)viewDidLoad {
-    [super viewDidLoad]; self.view.backgroundColor = [UIColor systemBackgroundColor]; [self setupTopBar]; [self setupContentView]; [self 
+    [super viewDidLoad]; self.view.backgroundColor = [UIColor systemBackgroundColor]; [self setupTopBar]; [self setupContentView]; [self
     showPage:PCLPageTypeLaunch];
 }
-- (void)setupTopBar { self.topBar = [[PCLTopBarView alloc] init]; self.topBar.delegate = self; self.topBar.translatesAutoresizingMaskIntoConstraints = NO; 
+- (void)setupTopBar { self.topBar = [[PCLTopBarView alloc] init]; self.topBar.delegate = self; self.topBar.translatesAutoresizingMaskIntoConstraints = NO;
     [self.view addSubview:self.topBar]; [NSLayoutConstraint activateConstraints:@[
-        [self.topBar.topAnchor constraintEqualToAnchor:self.view.topAnchor], [self.topBar.leadingAnchor 
+        [self.topBar.topAnchor constraintEqualToAnchor:self.view.topAnchor], [self.topBar.leadingAnchor
             constraintEqualToAnchor:self.view.leadingAnchor],
         [self.topBar.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor], [self.topBar.heightAnchor constraintEqualToConstant:64.0] ]];
 }
-- (void)setupContentView { self.contentView = [[UIView alloc] init]; self.contentView.translatesAutoresizingMaskIntoConstraints = NO; [self.view 
+- (void)setupContentView { self.contentView = [[UIView alloc] init]; self.contentView.translatesAutoresizingMaskIntoConstraints = NO; [self.view
     addSubview:self.contentView]; [NSLayoutConstraint activateConstraints:@[
-        [self.contentView.topAnchor constraintEqualToAnchor:self.topBar.bottomAnchor], [self.contentView.leadingAnchor 
+        [self.contentView.topAnchor constraintEqualToAnchor:self.topBar.bottomAnchor], [self.contentView.leadingAnchor
             constraintEqualToAnchor:self.view.leadingAnchor],
-        [self.contentView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor], [self.contentView.bottomAnchor 
+        [self.contentView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor], [self.contentView.bottomAnchor
             constraintEqualToAnchor:self.view.bottomAnchor]
-    ]]; self.pageLabel = [[UILabel alloc] init]; self.pageLabel.translatesAutoresizingMaskIntoConstraints = NO; self.pageLabel.font = [UIFont systemFontOfSize:30 
+    ]]; self.pageLabel = [[UILabel alloc] init]; self.pageLabel.translatesAutoresizingMaskIntoConstraints = NO; self.pageLabel.font = [UIFont systemFontOfSize:30
                           weight:UIFontWeightSemibold];
-    self.pageLabel.textColor = [UIColor labelColor]; [self.contentView addSubview:self.pageLabel]; [NSLayoutConstraint activateConstraints:@[ 
+    self.pageLabel.textColor = [UIColor labelColor]; [self.contentView addSubview:self.pageLabel]; [NSLayoutConstraint activateConstraints:@[
         [self.pageLabel.centerXAnchor
             constraintEqualToAnchor:self.contentView.centerXAnchor], [self.pageLabel.centerYAnchor constraintEqualToAnchor:self.contentView.centerYAnchor] ]];
 }
 - (void)topBarView:(PCLTopBarView *)topBar didSelectPage:(PCLPageType)page { [self showPage:page];
 }
-- (void)showPage:(PCLPageType)page { switch (page) { case PCLPageTypeLaunch: self.pageLabel.text = @"启动"; break; case PCLPageTypeDownload: self.pageLabel.text 
-            = @"下载"; break;
-        case PCLPageTypeMultiplayer: self.pageLabel.text = @"联机"; break; case PCLPageTypeSettings: self.pageLabel.text = @"设置"; break;
+- (void)showPage:(PCLPageType)page {
+    switch (page) {
+        case PCLPageTypeLaunch:
+            self.pageLabel.text = @"启动";
+            break;
+
+        case PCLPageTypeDownload:
+            self.pageLabel.text = @"下载";
+            break;
+
+        case PCLPageTypeMultiplayer:
+            self.pageLabel.text = @"联机";
+            break;
+
+        case PCLPageTypeSettings:
+            self.pageLabel.text = @"设置";
+            break;
     }
-- (BOOL)prefersStatusBarHidden { return YES;}
-}@end
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
+
+@end
