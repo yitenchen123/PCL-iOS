9e8b5df335f3bdf1fd55d061124309df8742ea11
Robit-space <207705032+Robit-space@users.noreply.github.com>
2026-08-18 11:23:11 +0800

    Refine splash logo vector geometry

--- Sources/App/PCLAppDelegate.m
+++ Sources/App/PCLAppDelegate.m
@@ -126,8 +126,9 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
 
     CGFloat unit = shortSide * 0.052;
-    CGFloat logoW = unit * 10.2;
-    CGFloat logoH = unit * 3.4;
+
+    CGFloat logoW = unit * 8.10;
+    CGFloat logoH = unit * 3.35;
 
     UIView *logoView = [[UIView alloc] initWithFrame:
         CGRectMake((w - logoW) / 2.0,
@@ -137,42 +138,90 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     [splash addSubview:logoView];
 
-
-    void (^block)(CGFloat,CGFloat,CGFloat,CGFloat) =
-    ^(CGFloat x, CGFloat y, CGFloat bw, CGFloat bh) {
-        UIView *v = [[UIView alloc] initWithFrame:
-            CGRectMake(x * unit, y * unit,
-                       bw * unit, bh * unit)];
-        v.backgroundColor = UIColor.whiteColor;
-        [logoView addSubview:v];
+    void (^addShape)(UIBezierPath *) =
+    ^(UIBezierPath *path) {
+        CAShapeLayer *layer = [CAShapeLayer layer];
+        layer.path = path.CGPath;
+        layer.fillColor = UIColor.whiteColor.CGColor;
+        layer.fillRule = kCAFillRuleEvenOdd;
+        [logoView.layer addSublayer:layer];
     };
 
+    UIBezierPath *pPath = [UIBezierPath bezierPath];
 
-    block(0.00, 0.00, 0.42, 2.10);
-    block(0.00, 0.00, 1.65, 0.42);
-    block(1.23, 0.00, 0.42, 1.25);
-    block(0.65, 0.83, 1.00, 0.42);
+    [pPath moveToPoint:CGPointMake(0.00 * unit, 0.00 * unit)];
+    [pPath addLineToPoint:CGPointMake(1.85 * unit, 0.00 * unit)];
+    [pPath addLineToPoint:CGPointMake(1.85 * unit, 1.32 * unit)];
+    [pPath addLineToPoint:CGPointMake(0.45 * unit, 1.32 * unit)];
+    [pPath addLineToPoint:CGPointMake(0.45 * unit, 2.25 * unit)];
+    [pPath addLineToPoint:CGPointMake(0.00 * unit, 2.25 * unit)];
+    [pPath closePath];
 
+    UIBezierPath *pHole =
+        [UIBezierPath bezierPathWithRect:
+            CGRectMake(0.45 * unit,
+                       0.43 * unit,
+                       0.96 * unit,
+                       0.47 * unit)];
 
-    block(2.15, 0.00, 0.42, 2.10);
-    block(2.15, 0.00, 1.85, 0.42);
-    block(2.15, 1.68, 1.85, 0.42);
+    [pPath appendPath:pHole];
+    addShape(pPath);
 
+    UIBezierPath *cPath = [UIBezierPath bezierPath];
 
-    block(4.45, 0.00, 0.42, 2.10);
-    block(4.45, 1.68, 1.70, 0.42);
+    [cPath moveToPoint:CGPointMake(2.25 * unit, 0.00 * unit)];
+    [cPath addLineToPoint:CGPointMake(4.15 * unit, 0.00 * unit)];
+    [cPath addLineToPoint:CGPointMake(4.15 * unit, 0.45 * unit)];
+    [cPath addLineToPoint:CGPointMake(2.70 * unit, 0.45 * unit)];
 
+    [cPath addLineToPoint:CGPointMake(2.70 * unit, 1.80 * unit)];
+    [cPath addLineToPoint:CGPointMake(4.15 * unit, 1.80 * unit)];
+    [cPath addLineToPoint:CGPointMake(4.15 * unit, 2.25 * unit)];
+    [cPath addLineToPoint:CGPointMake(2.25 * unit, 2.25 * unit)];
+    [cPath closePath];
 
-    block(1.05, -0.34, 0.20, 0.20);
-    block(1.32, -0.22, 0.15, 0.15);
-    block(0.82, -0.14, 0.12, 0.12);
+    addShape(cPath);
 
+    UIBezierPath *lPath = [UIBezierPath bezierPath];
+
+    [lPath moveToPoint:CGPointMake(4.55 * unit, 0.00 * unit)];
+    [lPath addLineToPoint:CGPointMake(5.00 * unit, 0.00 * unit)];
+    [lPath addLineToPoint:CGPointMake(5.00 * unit, 1.80 * unit)];
+    [lPath addLineToPoint:CGPointMake(6.35 * unit, 1.80 * unit)];
+    [lPath addLineToPoint:CGPointMake(6.35 * unit, 2.25 * unit)];
+    [lPath addLineToPoint:CGPointMake(4.55 * unit, 2.25 * unit)];
+    [lPath closePath];
+
+    addShape(lPath);
+
+    CGFloat mark = unit * 0.14;
+
+    NSArray *marks = @[
+        @[@0.18, @0.16],
+        @[@0.32, @0.30]
+    ];
+
+    for (NSArray *m in marks) {
+        UIView *dot = [[UIView alloc] initWithFrame:
+            CGRectMake([m[0] doubleValue] * unit,
+                       [m[1] doubleValue] * unit,
+                       mark,
+                       mark)];
+
+        dot.backgroundColor =
+            [UIColor colorWithRed:24.0/255.0
+                            green:111.0/255.0
+                             blue:232.0/255.0
+                            alpha:1.0];
+
+        [logoView addSubview:dot];
+    }
 
     UILabel *ios = [[UILabel alloc] initWithFrame:
-        CGRectMake(6.65 * unit,
-                   0.63 * unit,
-                   2.05 * unit,
-                   0.90 * unit)];
+        CGRectMake(6.12 * unit,
+                   1.27 * unit,
+                   1.72 * unit,
+                   0.82 * unit)];
 
     ios.text = @"iOS";
     ios.textAlignment = NSTextAlignmentCenter;
@@ -185,28 +234,26 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
                         alpha:1.0];
 
     ios.font =
-        [UIFont systemFontOfSize:unit * 0.62
+        [UIFont systemFontOfSize:unit * 0.57
                           weight:UIFontWeightSemibold];
 
-
-    ios.layer.cornerRadius = unit * 0.15;
+    ios.layer.cornerRadius = unit * 0.14;
     ios.clipsToBounds = YES;
+
     [logoView addSubview:ios];
 
-
     UILabel *subtitle = [[UILabel alloc] initWithFrame:
-        CGRectMake(0,
-                   2.55 * unit,
+        CGRectMake(0.0,
+                   2.63 * unit,
                    logoW,
-                   0.65 * unit)];
+                   0.58 * unit)];
 
     subtitle.text = @"P C L - i O S";
     subtitle.textAlignment = NSTextAlignmentCenter;
     subtitle.textColor = UIColor.whiteColor;
 
-
     subtitle.font =
-        [UIFont monospacedSystemFontOfSize:unit * 0.36
+        [UIFont monospacedSystemFontOfSize:unit * 0.33
                                     weight:UIFontWeightRegular];
 
     [logoView addSubview:subtitle];
